13e182478354ae8ad98d05c5a3377e399b516dd6
Marco Bassaletti <bassaletti@gmail.com>
2020-12-06 13:49:42 -0300

    Nginx support with self-signed ssl for local development.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['django-bookmarks.mbassale.cl', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['app', 'localhost', '127.0.0.1']
 
 
 # Application definition

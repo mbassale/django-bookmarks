34a441cf20cb1b565cc1fc36d6a6e0a01bfe3474
Marco Bassaletti <bassaletti@gmail.com>
2021-01-23 18:39:48 -0300

    Added redirects to dashboard for logged users.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- bookmarks/urls.py
+++ bookmarks/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', lambda request: redirect('/account/')),
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('social-auth/', include('social_django.urls')),
